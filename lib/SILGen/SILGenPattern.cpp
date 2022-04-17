71310962a811449e81a147701582d1d5eeab6661
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:43:13 -0400

    spelling: preparation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/SILGen/SILGenPattern.cpp
+++ lib/SILGen/SILGenPattern.cpp
@@ -2706,7 +2706,7 @@ static void switchCaseStmtSuccessCallback(SILGenFunction &SGF,
   }
 
   // Ok, at this point we know that we have a multiple entrance block. Grab our
-  // shared destination in preperation for branching to it.
+  // shared destination in preparation for branching to it.
   //
   // NOTE: We do not emit anything yet, since we will emit the shared block
   // later.
@@ -3050,7 +3050,7 @@ void SILGenFunction::emitCatchDispatch(DoCatchStmt *S, ManagedValue exn,
     }
 
     // Ok, at this point we know that we have a multiple entrance block. Grab
-    // our shared destination in preperation for branching to it.
+    // our shared destination in preparation for branching to it.
     //
     // NOTE: We do not emit anything yet, since we will emit the shared block
     // later.
