cc1ba375ed65d31f798e3c7c1d74b0e8a2a51bf6
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:48:24 -0400

    spelling: serialization
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -473,7 +473,7 @@ namespace swift {
     bool HermeticSealAtLink = false;
 
     /// Allow deserializing implementation only dependencies. This should only
-    /// be set true by lldb and other tooling, so that deserilization
+    /// be set true by lldb and other tooling, so that serialization
     /// recovery issues won't bring down the debugger.
     /// TODO: remove this when @_implementationOnly modules are robust enough.
     bool AllowDeserializingImplementationOnly = false;
