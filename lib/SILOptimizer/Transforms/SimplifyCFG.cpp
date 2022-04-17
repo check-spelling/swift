d9a4bfb779880d2ad445e54230388440a0f1648c
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:53:23 -0400

    spelling: ultimate
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/SILOptimizer/Transforms/SimplifyCFG.cpp
+++ lib/SILOptimizer/Transforms/SimplifyCFG.cpp
@@ -2075,7 +2075,7 @@ static bool onlyForwardsNone(SILBasicBlock *noneBB, SILBasicBlock *someBB,
 ///   \             |       v
 ///    \            ... more bbs? (A)
 ///     \           /
-///       ulimateBB
+///       ultimateBB
 ///
 /// This routine does not support diverging control flow in (A). This means that
 /// there must not be any loops or diamonds beginning in that region. We do
